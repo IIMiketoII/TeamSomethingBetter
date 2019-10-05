cb531f228233f9258bcfed599e3ccdc66c2d1acc
Henry Hazlett <ticotoad@gmail.com>
2019-10-04 21:17:43 -0500

    Climbing in progress

---
+++ TeamSomethingBetter/Assets/Climbing.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Climbing : MonoBehaviour
+{
+    public Rigidbody player;
+
+    void OnCollisionStay(Collision thing)
+    {
+        if (thing.collider.material.name == "Climb Material" && (Input.GetKey("d") || Input.GetKey("a")))
+        {
+            player.AddForce(transform.up * 20);
+        }
+    }
+}
