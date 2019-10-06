2c73483c416d7af9628e0431d5411fc1d5c3f0e4
Michael Benavides <mab160530@utdallas.edu>
2019-10-06 14:01:40 -0700

    Big boy slime animates

--- TeamSomethingBetter/Assets/Scripts/AnimationControl.cs
+++ TeamSomethingBetter/Assets/Scripts/AnimationControl.cs
@@ -27,15 +27,90 @@ public class AnimationControl : MonoBehaviour
     // Update is called once per frame
     void Update()
 
-    {   /*//Flip Character and running
+    {
 
-
-        else*/
-        if (this.GetComponent<leaderMovement>().isLeader == true)
+        if (this.GetComponent<leaderMovement>() != null)
         {
+
+            if (this.GetComponent<leaderMovement>().isLeader == true)
+            {
+                if (Input.GetKey("a"))
+                {
+                    slimeAnimation.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+                    animator.SetBool("IsIdle", false);
+                    animator.SetBool("IsRunning", true);
+                    animator.SetBool("IsJumping", false);
+                    moving = true;
+                }
+                if (Input.GetKey("d"))
+                {
+                    slimeAnimation.gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+                    animator.SetBool("IsIdle", false);
+                    animator.SetBool("IsRunning", true);
+                    animator.SetBool("IsJumping", false);
+                    moving = true;
+                }
+                if (!Input.GetKey("a") && !Input.GetKey("d"))
+                {
+                    moving = false;
+                }
+                if (moving == false && this.GetComponent<leaderMovement>().isGrounded == true)
+                {
+                    animator.SetBool("IsIdle", true);
+                    animator.SetBool("IsRunning", false);
+                    animator.SetBool("IsJumping", false);
+                }
+                if (this.GetComponent<leaderMovement>().isGrounded == false)
+                {
+                    animator.SetBool("IsIdle", false);
+                    animator.SetBool("IsRunning", false);
+                    animator.SetBool("IsJumping", true);
+                }
+
+
+            }
+
+            if (this.GetComponent<leaderMovement>().isLeader == false)
+            {
+                if (this.transform.position.x > currentXPosition && this.GetComponent<leaderMovement>().isGrounded)
+                {
+                    slimeAnimation.gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+                    currentXPosition = this.transform.position.x;
+                    Debug.Log("Going RIght");
+                    animator.SetBool("IsIdle", false);
+                    animator.SetBool("IsRunning", true);
+                    animator.SetBool("IsJumping", false);
+                }
+                if (this.transform.position.x < currentXPosition && this.GetComponent<leaderMovement>().isGrounded)
+                {
+                    slimeAnimation.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+                    currentXPosition = this.transform.position.x;
+                    Debug.Log("Going Left");
+                    animator.SetBool("IsIdle", false);
+                    animator.SetBool("IsRunning", true);
+                    animator.SetBool("IsJumping", false);
+                }
+
+                //Set Idle
+                if (this.transform.position.x == 0 && this.GetComponent<leaderMovement>().isGrounded)
+                {
+                    animator.SetBool("IsIdle", true);
+                    animator.SetBool("IsRunning", false);
+                    animator.SetBool("IsJumping", false);
+                }
+                //Set Jumping
+                if (this.GetComponent<leaderMovement>().isGrounded == false)
+                {
+                    animator.SetBool("IsIdle", false);
+                    animator.SetBool("IsRunning", false);
+                    animator.SetBool("IsJumping", true);
+                }
+            }
+        }
+            if(this.GetComponent<beegMovement>() != null){
             if (Input.GetKey("a"))
             {
-                slimeAnimation.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+                slimeAnimation.gameObject.transform.rotation = Quaternion.Euler(0, -95, 0);
                 animator.SetBool("IsIdle", false);
                 animator.SetBool("IsRunning", true);
                 animator.SetBool("IsJumping", false);
@@ -43,67 +118,29 @@ public class AnimationControl : MonoBehaviour
             }
             if (Input.GetKey("d"))
             {
-                slimeAnimation.gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+                slimeAnimation.gameObject.transform.rotation = Quaternion.Euler(0, 95, 0);
                 animator.SetBool("IsIdle", false);
                 animator.SetBool("IsRunning", true);
                 animator.SetBool("IsJumping", false);
                 moving = true;
             }
-            if(!Input.GetKey("a") && !Input.GetKey("d"))
+            if (!Input.GetKey("a") && !Input.GetKey("d"))
             {
                 moving = false;
             }
-            if(moving == false && this.GetComponent<leaderMovement>().isGrounded == true)
+            if (moving == false && this.GetComponent<beegMovement>().isGrounded == true)
             {
                 animator.SetBool("IsIdle", true);
                 animator.SetBool("IsRunning", false);
                 animator.SetBool("IsJumping", false);
             }
-            if(this.GetComponent<leaderMovement>().isGrounded == false)
+            if (this.GetComponent<beegMovement>().isGrounded == false)
             {
                 animator.SetBool("IsIdle", false);
                 animator.SetBool("IsRunning", false);
                 animator.SetBool("IsJumping", true);
             }
-
-
-        }
-
-        if (this.GetComponent<leaderMovement>().isLeader == false)
-        {
-            if (this.transform.position.x > currentXPosition && this.GetComponent<leaderMovement>().isGrounded)
-            {
-                slimeAnimation.gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
-                currentXPosition = this.transform.position.x;
-                Debug.Log("Going RIght");
-                animator.SetBool("IsIdle", false);
-                animator.SetBool("IsRunning", true);
-                animator.SetBool("IsJumping", false);
-            }
-            if (this.transform.position.x < currentXPosition && this.GetComponent<leaderMovement>().isGrounded)
-            {
-                slimeAnimation.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
-                currentXPosition = this.transform.position.x;
-                Debug.Log("Going Left");
-                animator.SetBool("IsIdle", false);
-                animator.SetBool("IsRunning", true);
-                animator.SetBool("IsJumping", false);
-            }
-
-            //Set Idle
-            if (this.transform.position.x == 0 && this.GetComponent<leaderMovement>().isGrounded)
-            {
-                animator.SetBool("IsIdle", true);
-                animator.SetBool("IsRunning", false);
-                animator.SetBool("IsJumping", false);
-            }
-            //Set Jumping
-            if (this.GetComponent<leaderMovement>().isGrounded == false)
-            {
-                animator.SetBool("IsIdle", false);
-                animator.SetBool("IsRunning", false);
-                animator.SetBool("IsJumping", true);
-            }
+            Debug.Log(moving);
         }
     }
 }
