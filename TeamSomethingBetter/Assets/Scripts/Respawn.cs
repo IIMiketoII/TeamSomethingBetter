1a4cbcfa08b420b0f33a4ce4a8b27952dee8c5c2
Henry Hazlett <ticotoad@gmail.com>
2019-10-06 01:28:11 -0500

    respawn script

---
+++ TeamSomethingBetter/Assets/Scripts/Respawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public Vector3 spawnPoint;
+
+    public void Spawn()
+    {
+        transform.position = spawnPoint;
+    }
+
+    void OnTriggerEnter(Collision col)
+    {
+        if (col.gameObject.tag == "checkpoint")
+        {
+            spawnPoint = col.transform.position;
+        }
+    }
+}
