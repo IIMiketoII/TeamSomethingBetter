1f89561d1e4163ce76cf32a823c1b60b0bb48088
Michael Benavides <mab160530@utdallas.edu>
2019-10-05 18:00:30 -0700

    Added in time dilation functionality

--- TeamSomethingBetter/Assets/Scripts/GameManager.cs
+++ TeamSomethingBetter/Assets/Scripts/GameManager.cs
@@ -9,17 +9,51 @@ public class GameManager : MonoBehaviour
     public GameObject blue;
     public GameObject green;
 
+    public bool toggleTime;
+    private float OGSpeed;
+
+    public float SlowDownAmount;
+    public float SlowDownAmountMax;
+
     // Start is called before the first frame update
     void Start()
     {
         red = GameObject.Find("slimeRed");
         blue = GameObject.Find("slimeBlue");
         green = GameObject.Find("slimeGreen");
+
+        OGSpeed = blue.GetComponent<leaderMovement>().speed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (toggleTime == true)
+        {
+            SlowDownAmount -= 1;
+            Debug.Log(SlowDownAmount);
+            if (SlowDownAmount <= 0)
+            {
+                SlowDownAmount = 0;
+            }
+        }
+        else if(toggleTime == false)
+        {
+            SlowDownAmount = SlowDownAmount + 0.1f;
+            Debug.Log(SlowDownAmount);
+            if (SlowDownAmount >= SlowDownAmountMax)
+            {
+                SlowDownAmount = SlowDownAmountMax;
+            }
+        }
+        if (SlowDownAmount <= 0)
+        {
+            Time.timeScale = 1;
+            Time.fixedDeltaTime = 0.02F;
+            toggleTime = false;
+        }
+
+        //Switch Character Functionality
         if (Input.GetButtonUp("switch"))
         {
             // Red is leader
@@ -46,6 +80,53 @@ public class GameManager : MonoBehaviour
                 red.GetComponent<leaderMovement>().isLeader = true;
                 Debug.Log("green to red succ");
             }
+        }
+        //Time Dialation
+        if (Input.GetKeyDown("f"))
+        {
+            if (SlowDownAmount > 0)
+            {
+                if (toggleTime == false)
+                {
+                    //Time.timeScale = 0.2f;
+                    DoSlowMotion();
+                    toggleTime = true;
+                }
+                else if (toggleTime == true)
+                {
+                    Time.timeScale = 1;
+                    Time.fixedDeltaTime = 0.02F;
+                    toggleTime = false;
+                }
+
+            }
+
+
+
+            /*
+            if(toggleTime == false)
+            {
+
+                toggleTime = true;
+                blue.GetComponent<leaderMovement>().speed = OGSpeed;
+                red.GetComponent<leaderMovement>().speed = OGSpeed;
+                green.GetComponent<leaderMovement>().speed = OGSpeed;
+                Debug.Log("RegularTime");
+            }
+            else
+            {
+                toggleTime = false;
+                blue.GetComponent<leaderMovement>().speed = OGSpeed/2;
+                red.GetComponent<leaderMovement>().speed = OGSpeed/2;
+                green.GetComponent<leaderMovement>().speed = OGSpeed/2;
+                Debug.Log("SlowTime");
+            }
+            */
+        }
+        void DoSlowMotion()
+        {
+            Time.timeScale = 0.2f;
+            Time.fixedDeltaTime = 0.02F * Time.timeScale;
         }
     }
 }
